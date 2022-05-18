6d992b99e89336dd7a50421b0ec0e2da87242fbe
Thanh Nguyen <chrisng9@outlook.com>
2022-05-18 05:05:00 -0700

    At it again, jost modify a few example and comment some code in primitive data type

--- sec2/01-Primitive/01-expl/showform.js
+++ sec2/01-Primitive/01-expl/showform.js
@@ -1,9 +1,14 @@
 // Primitive data type in JavaScript
 
-let name = "Marko";    // String
-let age = 39;          // Number
+let fname = "Marko";    // name is variable, "Marko" is string
+let age = 39;          // age is variable, 39 is number
 let showForm  = true;  // Boolean
 let PI = 3.1314;       // Float
 
 if (showForm) alert("show form is now on");
+
+console.log(fname);
+console.log(age);
+console.log(showForm);
+console.log(PI);
 
